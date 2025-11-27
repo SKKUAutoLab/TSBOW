0f3c9b5827fc71df8992d9f9d848abbcc16fcf8c
ngochdm <ngochdm@gmail.com>
2025-11-27 16:20:50 +0900

    update download_TSBOW.py

--- utils/download_TSBOW.py
+++ utils/download_TSBOW.py
@@ -8,5 +8,14 @@ def main():
         description="Download TSBOW dataset from Hugging Face."
     )
 
+    # The other versions of datasets will be added in the future
+    parser.add_argument(
+        "--repo_id",
+        type=str,
+        choices=["TSBOW"],
+        required=True,
+        help="Dataset type to download (TSBOW)",
+    )
+
 if __name__ == "__main__":
     main()
